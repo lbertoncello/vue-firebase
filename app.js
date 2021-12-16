076b72466da67253e55c6404db42ebaedc661e7c
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-16 18:43:27 -0300

    feat: first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
+})
+
+app.mount('#app')

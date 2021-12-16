74096a389ee51910105930124e5e387f536f7fa3
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-16 18:55:40 -0300

    feat: methods and click events

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
-        }
-    }
+  data() {
+    return {
+				title: 'The Final Empire',
+				author: 'Brandon Sanderson',
+				age: 45
+		}
+	},
+	methods: {
+		changeTitle(title) {
+			this.title = title
+		}
+	}
 })
 
 app.mount('#app')

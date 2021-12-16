8a9ffc88e2f39dae093f47d5cc913d956218840b
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-16 19:12:35 -0300

    class: conditional rendering

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-				title: 'The Final Empire',
-				author: 'Brandon Sanderson',
-				age: 45
+			showBooks: true,
+			title: 'The Final Empire',
+			author: 'Brandon Sanderson',
+			age: 45
 		}
 	},
 	methods: {
-		changeTitle(title) {
-			this.title = title
+		toggleShowBooks() {
+			this.showBooks = !this.showBooks
 		}
 	}
 })

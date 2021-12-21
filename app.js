b2ea33888e2ce4e6df8ac102c5e04cc275bea089
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-21 16:15:34 -0300

    class: other mouse events

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
 			showBooks: true,
 			title: 'The Final Empire',
 			author: 'Brandon Sanderson',
-			age: 45
+			age: 45,
+			x: 0,
+			y: 0
 		}
 	},
 	methods: {
 		toggleShowBooks() {
 			this.showBooks = !this.showBooks
+		},
+		handleEvent(event, data) {
+			console.log(event, event.type)
+			if(data) {
+				console.log(data)
+			}
+		},
+		handleMouseMove(event) {
+			this.x = event.offsetX
+			this.y = event.offsetY
 		}
 	}
 })

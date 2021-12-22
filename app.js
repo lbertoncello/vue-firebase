60bef7554c782ef08e02bc22b7c60e0bf88a5e81
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-22 15:37:14 -0300

    class: dynamic (conditional) classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
 			url: 'http://www.thenetninja.co.uk',
 			showBooks: true,
 			books: [
-				{ title: 'name of the wind', author: 'patrich rothfuss', img: './assets/1.jpg' },
-				{ title: 'the way of kings', author: 'brandon sanderson', img: './assets/2.jpg' },
-				{ title: 'the final empire', author: 'brandon sanderson', img: './assets/3.jpg' },
+				{ title: 'name of the wind', author: 'patrich rothfuss', img: './assets/1.jpg', isFav: true },
+				{ title: 'the way of kings', author: 'brandon sanderson', img: './assets/2.jpg', isFav: false },
+				{ title: 'the final empire', author: 'brandon sanderson', img: './assets/3.jpg', isFav: true },
 			]
 		}
 	},

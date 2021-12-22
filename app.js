e178b49efa2eebda87cf305d35fcee26eaa695e5
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-22 15:14:41 -0300

    class: outputting lists (v-for)

--- app.js
+++ app.js
@@ -2,27 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
 			showBooks: true,
-			title: 'The Final Empire',
-			author: 'Brandon Sanderson',
-			age: 45,
-			x: 0,
-			y: 0
+			books: [
+				{ title: 'name of the wind', author: 'patrich rothfuss' },
+				{ title: 'the way of kings', author: 'brandon sanderson' },
+				{ title: 'the final empire', author: 'brandon sanderson' },
+			]
 		}
 	},
 	methods: {
 		toggleShowBooks() {
 			this.showBooks = !this.showBooks
 		},
-		handleEvent(event, data) {
-			console.log(event, event.type)
-			if(data) {
-				console.log(data)
-			}
-		},
-		handleMouseMove(event) {
-			this.x = event.offsetX
-			this.y = event.offsetY
-		}
 	}
 })
 

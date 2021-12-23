c9fa08436ff90aae292aaca8990715e2e6ab4b5d
lbertoncello <lucas_b_andrade@hotmail.com>
2021-12-23 19:31:36 -0300

    class: computed properties

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ const app = Vue.createApp({
 		toggleFav(book) {
 			book.isFav = !book.isFav
 		}
+	},
+	computed: {
+		filteredBooks() {
+			return this.books.filter((book) => book.isFav)
+		}
 	}
 })
 
